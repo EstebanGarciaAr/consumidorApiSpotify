41735ebc6a2ec7b436aa79e409efa581a7e07b0a
EstenamGarciaAr <jestebangarciac23@gmail.com>
2025-05-02 01:42:46 -0500

    Creacion del hook Authenticate

---
+++ consumidorApiSpotify/src/auth/hooks/useAuthenticate.js
@@ -0,0 +1,25 @@
+import { authTypes } from "../types/authTypes";
+
+export const useAuthenticate = (dispatch) => {
+    //login
+
+    const login = ({email, password}) => {
+        const action = {
+            type: authTypes.login,
+            payload: {email, password}
+        };
+
+        localStorage.setItem('user',JSON.stringify({email,password}))
+
+        dispatch(action)
+    }
+    //logout
+
+    const logout = () => {
+        const action = {
+            type: authTypes.logout
+        }
+        dispatch(action);
+    }
+    return {login, logout};
+}
